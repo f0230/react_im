f456a220a2792a996da3e7f77c92288bca0a15c2
f0230 <f02301111@gmail.com>
2025-04-25 22:35:59 -0300

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -17,4 +17,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
