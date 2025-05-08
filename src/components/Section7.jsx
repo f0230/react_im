c4f930129ebf02e6004daa6a66fa3dfdebdc68ed
f0230 <f02301111@gmail.com>
2025-05-07 22:26:45 -0300

    we

--- src/components/Section7.jsx
+++ src/components/Section7.jsx
@@ -13,7 +13,7 @@ const Section7 = () => {
                 patternScaleX={1}
                 patternScaleY={1}
                 patternRefreshInterval={2}
-                patternAlpha={35}
+                patternAlpha={20}
             /> 
             <div className="jrelative z-10 w-full mx-auto h-full flex items-center justify-center">
 
@@ -24,10 +24,10 @@ const Section7 = () => {
                 
                 <p className="font-product font-normal leading-[1.2]">  Nos adaptamos a tus necesidades y presupuesto. Creemos que, sin importar el alcance del proyecto, siempre podemos trabajar juntos para obtener resultados excepcionales. Nuestro enfoque flexible y comprometido garantiza que cada detalle se ajuste a tus expectativas, asegurando que logremos el mejor resultado, sin comprometer la calidad ni la innovación</p>
                     <div className="mt-4 md:mt-8 inline-block space-x-1 md:space-x-2 lg:space-x-3">
-                        <button className="text-[12px] md:text-[17px] w-[114px] h-[34px] md:w-[165px] md:h-[42px] bg-skyblue text-white rounded-[37px] shadow-lg hover:bg-skyblue/95 hover:text-white transition duration-300">
+                        <button className="text-[12px] md:text-[17px] w-[114px] h-[34px] md:w-[165px] md:h-[42px] bg-skyblue text-white rounded-[37px]  hover:bg-skyblue/95 hover:text-white transition duration-300">
                             Contactanos
                         </button>
-                        <button className="text-[12px] md:text-[17px] w-[114px] h-[34px] md:w-[165px] md:h-[42px] bg-white text-skyblue rounded-[37px] shadow-lg hover:bg-white/95 hover:text-skyblue transition duration-300">
+                        <button className="text-[12px] md:text-[17px] w-[114px] h-[34px] md:w-[165px] md:h-[42px] bg-white text-skyblue rounded-[37px]  hover:bg-white/95 hover:text-skyblue transition duration-300">
                             Más info
                         </button>
                     </div>         
