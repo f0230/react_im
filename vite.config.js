ebfb73233f1ccf7aea48c5b93343df38a620aba6
f0230 <f02301111@gmail.com>
2025-05-12 13:15:26 -0300

    we

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      filename: './dist/stats.html',
-      open: true,
-      gzipSize: true,
-      brotliSize: true
-    }),
-  ],
-  base: './',
+  plugins: [react()],
+  base: './', // Rutas relativas
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) return 'vendor';
-          if (id.includes('/src/components/')) return 'components';
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'framer-motion', 'gsap'],
   },
 });
