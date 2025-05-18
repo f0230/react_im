958847fdd1f3cde2027651410b60002aa8a16a66
f0230 <fran.salisg@gmail.com>
2025-05-18 01:52:10 -0300

    ultima

---
+++ src/utils/phone-validation.js
@@ -0,0 +1,11 @@
+// src/utils/phone-validation.js
+export const isValidPhone = (phone) => {
+    // Permite formatos internacionales (con o sin +), espacios y/o guiones opcionales
+    const phoneRegex = /^(\+?[0-9]{1,4})?[-\s]?([0-9]{3,4})[-\s]?([0-9]{3,4})[-\s]?([0-9]{0,4})$/;
+
+    // Elimina espacios, guiones y paréntesis para verificar longitud mínima
+    const digitsOnly = phone.replace(/[\s\-()]/g, '');
+    const hasMinimumDigits = digitsOnly.length >= 8;
+
+    return phoneRegex.test(phone) && hasMinimumDigits;
+};
